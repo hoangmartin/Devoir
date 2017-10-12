c43c249118929861114e607e3199bd7a0dcc6d74
hoangmartin <hoang@lacroixrouge-brest.fr>
2017-10-12 14:09:17 +0200

    Modification client_UDP.py

--- client_UDP.py
+++ client_UDP.py
@@ -2,20 +2,25 @@
 
 import socket
 
+#declarer IP, port du client et message
+
+client="192.168.0.202"
+port=5005
+message="cinema"
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.settimeout(1.0)
-
-sock.connect(???)
-sock.send(???)
+#
+sock.connect(client,port)
+sock.sendto(message,(client,port))
 
 
 trameReponse, addr = sock.recvfrom(1024)
 
 print "Réception de la trame de réponse", trameReponse.encode("hex")
 
-code = ???
+code = (ord(b0)<<24)+(ord(b0)<<16)+(ord(b0)<<8)+(ord(b0)<<0)
 
 print code
 
